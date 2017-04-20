66e76c50073c1e44acbeff699c755890a4eaf2fa
theTwist84 <lee.foulger.94@gmail.com>
2017-04-20 02:51:58 +0100

    2:51:58.72

--- src/Console/TextInput.h
+++ src/Console/TextInput.h
@@ -25,32 +25,26 @@ public:
 
 	void backspace()
 	{
-		if (currentPointerIndex > 0) {
+		if (currentPointerIndex > 0)
 			currentInput.erase(--currentPointerIndex, 1);
-		}
 	}
 
 	void del()
 	{
-		if (currentPointerIndex < currentInput.size()) {
+		if (currentPointerIndex < currentInput.size())
 			currentInput.erase(currentPointerIndex, 1);
-		}
 	}
 
 	void left()
 	{
 		if (currentPointerIndex > 0)
-		{
 			currentPointerIndex--;
-		}
 	}
 
 	void right()
 	{
 		if (currentPointerIndex < currentInput.size())
-		{
 			currentPointerIndex++;
-		}
 	}
 
 	void set(std::string basic_string)
